3c72705db098644e7cd50f0ffc827be182e575df
Marcel Verkerk <mverkerk@godaddy.com>
2018-02-01 23:19:21 -0700

    Update documentation + config fields + version

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # timings-demo
+
 Sample tests scripts to demonstrate the [timings API](https://www.github.com/godaddy/timings). You can copy/reference the test scripts in this repo to build your own "timings-enabled" scripts. You can find both JavaScript and Python based scripts in the `/js` and `/py` folders.
+
+The scripts are implementing two of the timings API clients that are available here:
+
+* `npm` installable JavaScript client [(http://www.github.com/godaddy/timings-client-js)](http://www.github.com/godaddy/timings-client-js)
+* `pip` installable Python client [(http://www.github.com/godaddy/timings-client-py)](http://www.github.com/godaddy/timings-client-py)
+
+## Installation
 
 You can also run the tests in this repo to test your **timings API**! See installation instructions below:
 
-## Installation
-If you actually want to run the test scripts:
+### **JavaScript based test scripts**
 
-* **JavaScript:** clone repo with `git clone` and install dependencies with `npm i` (**run this from inside the `/js` sub-folder!**)
+clone repo with `git clone` and install dependencies with `npm i` (**run this from inside the `/js` sub-folder!**)
 
 ```bash
 $ git clone https://github.com/Verkurkie/timings-demo.git
@@ -14,15 +21,8 @@ $ cd {rootFolder}/js
 $ npm i
 ```
 
-* **Python:** clone repo with `git clone` and install dependencies with `pip`:
+#### Configuration for JavaScript tests
 
-```bash
-$ git clone https://github.com/Verkurkie/timings-demo.git
-$ cd {rootFolder}/py
-$ pip install timingsclient selenium
-```
-
-## Configuration for JavaScript tests
 Before running tests, you have to configure the default parameters by editing the config file `{rootFolder}/js/.perfconfig.js`. The most important settings to change are:
 
 |key|description|example|
@@ -35,7 +35,7 @@ Before running tests, you have to configure the default parameters by editing th
 |`api_params.log.env_target`|Environment of the target app/api|`prod`|
 |`api_params.log.team`|The name of your team/product|`MY_PRODUCT`|
 
-## Running JavaScript tests (uses webdriverIO)
+#### Running JavaScript tests (uses webdriverIO)
 
 To run the all of the JS tests, run the following command:
 
@@ -51,7 +51,18 @@ $ cd {rootFolder}/js
 $ ./node_modules/.bin/wdio ./wdio.conf.js --spec ./specs/{spec file}.js
 ```
 
-## Configuration for Python tests
+### **Python based test scripts**
+
+clone repo with `git clone` and install dependencies with `pip`:
+
+```bash
+$ git clone https://github.com/Verkurkie/timings-demo.git
+$ cd {rootFolder}/py
+$ pip install timingsclient selenium
+```
+
+#### Configuration for Python tests
+
 Before running tests, you have to configure the default parameters by editing the config file `{rootFolder}/py/config.yaml`. The most important settings to change are:
 
 |key|description|example|
@@ -64,7 +75,8 @@ Before running tests, you have to configure the default parameters by editing th
 |`api_params.log.env_target`|Environment of the target (usually dev|test|prod)|`prod`|
 |`api_params.log.team`|The name of your team/product|`MY_PRODUCT`|
 
-## Running Python test
+#### Running Python test
+
 Then, to run the Python tests, simply run:
 
 ```bash
--- js/.perfconfig.js
+++ js/.perfconfig.js
@@ -1,22 +1,19 @@
 /*
-This file contains the default parameters for the API
-Below values will be used if they are not provided by the test script!
-
-Please set PERF_API_URL to the correct address of the API!!
-
+This file contains default parameters which will be used if they are not provided by the test script!
+Please set PERF_API_URL to the correct address of your timings API!!
 */
 
 module.exports = {
     // Below is the full fqdn of the API server
-    "PERF_API_URL": "http://timings-api.int.godaddy.com/v2/api/cicd/",             // The full URL to the API host
-    "api_timeout": 3000,                            // Timeout for the API - increase this if you're experiencing slow connection to the API
+    "PERF_API_URL": "http://localhost/v2/api/cicd/",             // The full URL to the API host
+    "api_timeout": 3000,                        // Timeout for the API - increase this if you're experiencing slow connection to the API
 
     // Below are your default "api parameters" that will be send to the API in the POST body - you can overwrite these with the `getApiParams()` method
     "api_params": {
-        "sla": {                // This is the "flat SLA" - valid keys are "pageLoadTime" or "visualCompleteTime"
+        "sla": {                                // This is the "flat SLA" - valid keys are "pageLoadTime" or "visualCompleteTime"
             "pageLoadTime": 2000                    // Flat maximum threshold - this will be used if baseline (+ padding) is greater or flags.assertBaseline=false!
         },
-        "baseline": {           // These settings will be used by the API to calculate the baseline
+        "baseline": {                           // These settings will be used by the API to calculate the baseline
             "days": 7,                              // Number of days to calculate the baseline for
             "perc": 75,                             // Percentile to calculate
             "padding": 1.2,                         // Extra padding on top of the calculated baseline (gives some wiggle-room)
@@ -25,19 +22,19 @@ module.exports = {
             },
             searchUrl: "*wildcard*"                 // Filter the baseline on the URL using this wildcard (if omitted, the main URL will be used)
         },
-        "flags": {              // These booleans will tell the API what to do
+        "flags": {                              // These booleans will tell the API what to do
             "assertBaseline": true,                 // Whether or not to compare against baseline
             "debug": false,                         // Request extra debug info from the API
             "esTrace": false,                       // Request elasticsearch output from API
             "esCreate": false,                      // Save results to elasticsearch
             "passOnFailedAssert": false             // Pass the test, even when the performance is above the threshold
         },
-        "log": {                // These key-value pairs will be stored in elasticsearch and can be used to slice & dice the data in Kibana
-            "app_info": "Sample test_info-JS",     // Info about the test(-step)
-            "platform": "Sample tester",          // Environment of the test machine (local, saucelabs, selenium grid, etc.)
+        "log": {                                // These key-value pairs will be stored in elasticsearch and can be used to slice & dice the data in Kibana
+            "test_info": "Sample test_info-JS",     // Info about the test(-step)
+            "env_tester": "Sample tester",          // Environment of the test machine (local, saucelabs, selenium grid, etc.)
             "browser": "Sample browser",            // Browser used to run the test with
-            "environment": "Sample target",          // Environment of the target app (usually dev, test, or prod)
-            "team": "DEMO"                          // THe name of the (test-)team 
+            "env_target": "Sample target",          // Environment of the target app (usually dev, test, or prod)
+            "team": "DEMO TEAM"                     // The name of the (test-)team 
         }
     }
 };
